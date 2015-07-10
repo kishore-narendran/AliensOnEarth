d8aed857ef56ff676267e2d93378bd463935d12f
Kishore Narendran <kishore.narendran09@gmail.com>
2015-07-10 13:14:05 +0700

    Modularized add-generator.js file

--- add-generator.js
+++ add-generator.js
@@ -15,6 +15,46 @@ var moveGenerator = function(err, result) {
 	}
 };
 
+var checkIfFileExists = function(plugin) {
+	if(!fs.existsSync(plugin)) {
+		console.log('Plugin file not found!'.red);
+		return false;
+	}
+	return true;
+};
+
+var checkIfPluginValid = function(plugin) {
+	if(options.plugin.substring(options.plugin.lastIndexOf('.')) != '.js') {
+		console.log('Generator file not a .js file!'.red);
+		return false;
+	}
+	return true;
+};
+
+var checkIfGeneratorExistsAndAdd = function(format, plugin) {
+	var filePath = path.join(__dirname, 'generators', 'generate-' + format + '.js');
+	//Checking if the Generator exists
+	if(fs.existsSync(filePath)) {
+		prompt.get(['Another generator exists for this format, replace it? (Y/N)'], function(err, result) {
+			if(err) {
+				console.log(err);
+				process.exit();
+			}
+			else if(result['Another generator exists for this format, replace it? (Y/N)'] == 'Y'){
+				fs.unlinkSync(filePath);
+				fs.rename(plugin, filePath, moveGenerator);
+			}
+			else {
+				console.log('Generator not added!'.red);
+				process.exit();
+			}
+		});
+	}
+	else {
+		fs.rename(plugin, filePath, moveGenerator);
+	}
+};
+
 var cli = commandLineArguments([
 	{name: 'format', type: String, alias: 'f'},
 	{name: 'plugin', type: String, alias: 'p'}
@@ -22,38 +62,12 @@ var cli = commandLineArguments([
 
 var options = cli.parse();
 
-if(!fs.existsSync(options.plugin)) {
-	console.log('Plugin file not found!'.red);
-	process.exit();
+if(checkIfFileExists(options.plugin) && checkIfPluginValid(options.plugin)) {
+	checkIfGeneratorExistsAndAdd(options.format, options.plugin);
 }
 
-//Checking if the plugin file is a .js file
-if(options.plugin.substring(options.plugin.lastIndexOf('.')) != '.js') {
-	console.log('Generator file not a .js file!'.red);
-	process.exit();
-}
 
-var filePath = path.join(__dirname, 'generators', 'generate-' + options.format + '.js');
 
-//Checcking if the Generator exists
-if(fs.existsSync(filePath)) {
-	prompt.get(['Another file exists for this format, replace it? (Y/N)'], function(err, result) {
-		if(err) {
-			console.log(err);
-			process.exit();
-		}
-		else if(result['Another file exists for this format, replace it? (Y/N)'] == 'Y'){
-			fs.unlinkSync(filePath);
-			fs.rename(options.plugin, filePath, moveGenerator);
-		}
-		else {
-			console.log('Generator not added!'.red);
-			process.exit();
-		}
-	});
-}
-else {
-	fs.rename(options.plugin, filePath, moveGenerator);
-}
+
 
 
